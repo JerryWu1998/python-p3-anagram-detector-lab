4995f67e50c66848b26170f56195bdbb3ef931f8
Yu Wu <yw3748@columbia.edu>
2023-07-07 14:55:42 -0400

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        result = []
+        for vocab in list:
+            sort_vocab = sorted(vocab)
+            if sort_vocab == sorted(self.word):
+                result.append(vocab)
+
+        return result
